0b41e9711eba021eb397015f5ca73efb79c03ad1
John Brunton <john.brunton@streetcar.co.uk>
2011-12-21 18:08:42 +0000

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+users = User.create([
+    { :user_name => 'jbrunton' },
+    { :user_name => 'irene' }
+])
+
+user_jbrunton = users[0]
+user_irene = users[1]
+
+blogs = Blog.create([
+    { :title => 'My Blog',      :user => user_jbrunton },
+    { :title => "Irene's Blog", :user => user_irene }
+])
+
+blog_jbrunton = blogs[0]
+blog_irene = blogs[1]
+
+blogPosts = BlogPost.create([
+    { :blog => blog_jbrunton,   :title => 'Some Post',      :content => '<p>Some content</p>' },
+    { :blog => blog_jbrunton,   :title => 'Another Post',   :content => '<p>Some content</p>' },
+    { :blog => blog_irene,      :title => "Irene's Post",   :content => '<p>Some content</p>' }
+])
