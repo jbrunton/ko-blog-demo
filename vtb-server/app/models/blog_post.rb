8ba4624d326107da88efd46ffe6812407fcce2f5
John Brunton <john.brunton@streetcar.co.uk>
2011-12-21 13:23:34 +0000

    improved BlogPost.search to scan content too

--- vtb-server/app/models/blog_post.rb
+++ vtb-server/app/models/blog_post.rb
@@ -2,6 +2,6 @@ class BlogPost < ActiveRecord::Base
     belongs_to :blog
     
     def self.search(pattern)
-        BlogPost.where("title like ?", pattern)
+        BlogPost.where("title like :pattern or content like :pattern", { :pattern => pattern})
     end
 end
