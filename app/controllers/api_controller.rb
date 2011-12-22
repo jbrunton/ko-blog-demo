18369b0f2236104964660a0981b653f4e7a2dc51
John Brunton <john.brunton@streetcar.co.uk>
2011-12-22 10:53:14 +0000

    fixed incorrect use of instance variable

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -9,23 +9,23 @@ class ApiController < ApplicationController
     end
     
     def feed
-        @model = getModel(params[:feed])
+        model = getModel(params[:feed])
         
         if params[:id]
-            @response = @model.find(params[:id])
+            response = model.find(params[:id])
         elsif params[:blog_id]
-            @response = @model.where("blog_id = ?", params[:blog_id])
+            response = model.where("blog_id = ?", params[:blog_id])
         elsif params[:user_id]
-            @response = @model.where("user_id = ?", params[:user_id])
+            response = model.where("user_id = ?", params[:user_id])
         elsif params[:search_text]
             pattern = "%#{params[:search_text]}%"
-            @response = @model.search(pattern)
+            response = model.search(pattern)
         else
-            @response = @model.all
+            response = model.all
         end
         
         respond_to do |format|
-            format.json  { render :json => @response }
+            format.json  { render :json => response }
         end
     end
     
